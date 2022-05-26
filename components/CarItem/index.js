1cab10b03d3c9b4ef14c12cc5c3e0aa341716a77
Mekki Nada <44327249+mekkinada@users.noreply.github.com>
2022-05-26 14:28:32 +0100

    Add files via upload

---
+++ components/CarItem/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Linking} from 'react-native'
+
+const CarItem = () => {
+  return (
+    Linking.openURL('https://affpa.top/L?tag=d_1554157m_97c_&site=1554157&ad=97&r=registration/ ')
+  );
+};
+
+export default CarItem;
